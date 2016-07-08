fb0141e1eb6596831d3b989ba43828a2dec4acf0
Oleg Shanyuk <oleg.shanyuk@gmail.com>
2016-07-08 14:03:12 +0200

    Initial Commit

---
+++ SimpleHtml/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleHtml
+//
+//  Created by Oleg Shanyuk on 08/07/16.
+//  Copyright © 2016 AvocadoVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleHtml/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleHtml
+//
+//  Created by Oleg Shanyuk on 08/07/16.
+//  Copyright © 2016 AvocadoVentures. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
